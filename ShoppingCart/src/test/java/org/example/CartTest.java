9257f3baba62147e0c1f2b4e8b38bfcbd9c77a90
Akshay Khanna <akshay9911102163@gmail.com>
2022-12-17 07:00:04 +0530

    Add files via upload

---
+++ ShoppingCart/src/test/java/org/example/CartTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CartTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    void name() {
+    }
+}
